4bcade15c1d04b7bb4508c4cad5ea44888cebc91
Bhushan Asati <bhushanasati25@gmail.com>
2025-02-15 21:45:26 -0500

    Code Updated

--- Basic Maths/Check for prime number.cpp
+++ Basic Maths/Check for prime number.cpp
@@ -17,3 +17,19 @@ public:
 
     }
 };
+
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    Solution sol;
+    if (sol.isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
