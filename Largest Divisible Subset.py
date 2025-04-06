2f3cdd4231e991fe4b8f38bb8712c855df4a08b4
Bhushan Asati <bhushanasati25@gmail.com>
2025-04-06 03:05:54 -0400

    File Updated

--- Largest Divisible Subset.py
+++ Largest Divisible Subset.py
@@ -12,23 +12,18 @@ class Solution(object):
     
         dp = [1] * n  
         prev = [-1] * n  
-        max_idx = 0  # To track the index of the largest subset's last element
-        
-        # Build up the dp and prev arrays.
+        max_idx = 0  
+    
         for i in range(n):
             for j in range(i):
-                # Check if the current number is divisible by nums[j]
                 if nums[i] % nums[j] == 0 and dp[j] + 1 > dp[i]:
                     dp[i] = dp[j] + 1
                     prev[i] = j
-            # Update max_idx if we found a larger subset ending at i.
             if dp[i] > dp[max_idx]:
                 max_idx = i
-        
-        # Reconstruct the largest divisible subset.
         result = []
         while max_idx != -1:
             result.append(nums[max_idx])
             max_idx = prev[max_idx]
         
-        return result[::-1]  # Reverse the list to get the correct order
+        return result[::-1]  
