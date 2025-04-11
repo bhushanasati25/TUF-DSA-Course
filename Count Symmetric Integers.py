26005ce178b9175f585f8721287b5604e4d3f12d
Bhushan Asati <bhushanasati25@gmail.com>
2025-04-11 00:28:04 -0400

    Code Updated

---
+++ Count Symmetric Integers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countSymmetricIntegers(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        count = 0
+        for num in range(low, high + 1):
+            s = str(num)
+            if len(s) % 2 == 0:
+                mid = len(s) // 2
+                left_sum = sum(int(d) for d in s[:mid])
+                right_sum = sum(int(d) for d in s[mid:])
+                if left_sum == right_sum:
+                    count += 1
+        return count
