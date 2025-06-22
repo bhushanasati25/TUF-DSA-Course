489e5607ddddb6f2b3284b9b0ff790495f0bf09b
Bhushan Asati <bhushanasati25@gmail.com>
2025-06-22 03:07:52 -0400

    Code Updated

---
+++ Divide a String Into Groups of Size k.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def divideString(self, s, k, fill):
+        """
+        :type s: str
+        :type k: int
+        :type fill: str
+        :rtype: List[str]
+        """
+        res = []
+        for i in range(0, len(s), k):
+            chunk = s[i:i+k]
+            if len(chunk) < k:
+                chunk += fill * (k - len(chunk))
+            res.append(chunk)
+        return res
