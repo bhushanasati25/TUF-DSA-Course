c6589db23c95b853ee405c4fbbda7b592dc0de29
Bhushan Asati <bhushanasati25@gmail.com>
2025-07-14 01:25:22 -0400

    Code Updated

---
+++ .vscode/Convert Binary Number in a Linked List to Integer.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: int
+        """
+        result = 0
+        current = head
+        while current:
+            result = (result << 1) | current.val
+            current = current.next
+        return result
