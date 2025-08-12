16f3af9d495aae95726fbf6d75eaa468d2c5f9b5
Bhushan Asati <bhushanasati25@gmail.com>
2025-08-11 23:25:41 -0400

    Code updated

---
+++ .vscode/Ways to Express an Integer as Sum of Powers.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numberOfWays(self, n, x):
+        MOD = 10**9 + 7
+        
+        max_base = 1
+        while (max_base ** x) <= n:
+            max_base += 1
+        max_base -= 1
+
+        memo = {}
+
+        def dfs(remaining, current):
+            if remaining == 0:
+                return 1
+            if remaining < 0 or current > max_base:
+                return 0
+
+            if (remaining, current) in memo:
+                return memo[(remaining, current)]
+
+            include = dfs(remaining - current**x, current + 1)
+            exclude = dfs(remaining, current + 1)
+
+            memo[(remaining, current)] = (include + exclude) % MOD
+            return memo[(remaining, current)]
+
+        return dfs(n, 1)
