221bc3a4a186daf6e1fda2770c16b01fb4f7c0b9
Bhushan Asati <bhushanasati25@gmail.com>
2025-09-08 02:08:59 -0400

    Code update

---
+++ .vscode/Convert Integer to the Sum of Two No-Zero Integers.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getNoZeroIntegers(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        def has_zero(x):
+            # check if number contains digit '0'
+            return '0' in str(x)
+        
+        for a in range(1, n):
+            b = n - a
+            if not has_zero(a) and not has_zero(b):
+                return [a, b]
