8de32e6d5d358e5909c068603f78e29907d1293d
Bhushan Asati <bhushanasati25@gmail.com>
2025-11-14 00:05:57 -0500

    Code Updated

---
+++ .vscode/Increment Submatrices by One.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def rangeAddQueries(self, n, queries):
+        """
+        :type n: int
+        :type queries: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        diff = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for r1, c1, r2, c2 in queries:
+            diff[r1][c1] += 1
+            diff[r1][c2 + 1] -= 1
+            diff[r2 + 1][c1] -= 1
+            diff[r2 + 1][c2 + 1] += 1
+
+        ans = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if i > 0:
+                    diff[i][j] += diff[i - 1][j]
+                if j > 0:
+                    diff[i][j] += diff[i][j - 1]
+                if i > 0 and j > 0:
+                    diff[i][j] -= diff[i - 1][j - 1]
+                ans[i][j] = diff[i][j]
+
+        return ans
