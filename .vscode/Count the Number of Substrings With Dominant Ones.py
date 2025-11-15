0b24c073cc3cf3440aff511968afe278339e59df
Bhushan Asati <bhushanasati25@gmail.com>
2025-11-15 00:45:15 -0500

    Code Updated

---
+++ .vscode/Count the Number of Substrings With Dominant Ones.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def numberOfSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        nxt = [n] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            nxt[i] = nxt[i + 1]
+            if s[i] == '0':
+                nxt[i] = i
+
+        ans = 0
+
+        for i in range(n):
+            cnt0 = 1 if s[i] == '0' else 0
+            j = i
+
+            while j < n and cnt0 * cnt0 <= n:
+                length_up_to_next_zero = nxt[j + 1] - i
+                cnt1 = length_up_to_next_zero - cnt0 
+
+                if cnt1 >= cnt0 * cnt0:
+                    ans += min(nxt[j + 1] - j, cnt1 - cnt0 * cnt0 + 1)
+
+                j = nxt[j + 1]
+                cnt0 += 1
+
+        return ans
