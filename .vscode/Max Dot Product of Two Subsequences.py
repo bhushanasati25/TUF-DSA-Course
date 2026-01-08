0af5d62746cb81a7cd0ddf8489c99b4f04e9b951
Bhushan Asati <bhushanasati25@gmail.com>
2026-01-08 05:56:43 -0500

    Code Updated

---
+++ .vscode/Max Dot Product of Two Subsequences.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxDotProduct(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        m, n = len(nums1), len(nums2)
+        
+        dp = [[float('-inf')] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                product = nums1[i - 1] * nums2[j - 1]
+                
+                dp[i][j] = max(
+                    dp[i - 1][j],              
+                    dp[i][j - 1],           
+                    max(0, dp[i - 1][j - 1]) + product  
+                )
+        
+        return dp[m][n]
