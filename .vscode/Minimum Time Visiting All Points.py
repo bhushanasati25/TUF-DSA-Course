c7b2dbb5d3658773848eec50d9d6d0037fca7567
Bhushan Asati <bhushanasati25@gmail.com>
2026-01-12 01:16:03 -0500

    Code updated

---
+++ .vscode/Minimum Time Visiting All Points.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        total_time = 0
+        for i in range(len(points) - 1):
+            x1, y1 = points[i]
+            x2, y2 = points[i+1]
+            
+            dx = abs(x2 - x1)
+            dy = abs(y2 - y1)
+            total_time += max(dx, dy)
+        
+        return total_time
+        
