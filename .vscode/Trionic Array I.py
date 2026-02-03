98a78c828e9fdf13b42d42edd0fefeb448405772
Bhushan Asati <bhushanasati25@gmail.com>
2026-02-03 00:54:38 -0500

    Code Updated

---
+++ .vscode/Trionic Array I.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isTrionic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        if n < 4:
+            return False
+
+        i = 0
+
+        while i + 1 < n and nums[i] < nums[i + 1]:
+            i += 1
+        if i == 0:   
+            return False
+
+        j = i
+        while j + 1 < n and nums[j] > nums[j + 1]:
+            j += 1
+        if j == i or j == n - 1: 
+            return False
+
+        while j + 1 < n and nums[j] < nums[j + 1]:
+            j += 1
+
+        return j == n - 1
