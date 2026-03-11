60d935d113e83c987a34bde1ef9be31de3fc6dc7
Bhushan Asati <bhushanasati25@gmail.com>
2026-03-11 00:14:39 -0400

    Code Updated

---
+++ Daily Problem/Complement of Base 10 Integer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def bitwiseComplement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 1
+        
+        mask = 0
+        temp = n
+        
+        while temp > 0:
+            mask = (mask << 1) | 1
+            temp >>= 1
+            
+        return n ^ mask
