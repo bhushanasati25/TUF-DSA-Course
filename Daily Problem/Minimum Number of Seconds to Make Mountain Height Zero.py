7a3e3368e670d36cd9c2deb8d876e0ca2c59bbe4
Bhushan Asati <bhushanasati25@gmail.com>
2026-03-13 00:49:09 -0400

    Code Updated

---
+++ Daily Problem/Minimum Number of Seconds to Make Mountain Height Zero.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def minNumberOfSeconds(self, mountainHeight, workerTimes):
+        """
+        :type mountainHeight: int
+        :type workerTimes: List[int]
+        :rtype: int
+        """
+        def canFinish(T):
+            total = 0
+
+            for w in workerTimes:
+                x = int((-1 + math.sqrt(1 + 8 * T / w)) // 2)
+                total += x
+
+                if total >= mountainHeight:
+                    return True
+
+            return False
+
+        left = 1
+        right = min(workerTimes) * mountainHeight * (mountainHeight + 1) // 2
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if canFinish(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+        
