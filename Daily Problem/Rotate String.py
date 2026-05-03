ebe22c90d2b4f3269172d268c6eb91c5118e8fb7
Bhushan Asati <bhushanasati25@gmail.com>
2026-05-03 03:26:05 -0400

    Code Updated

---
+++ Daily Problem/Rotate String.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+
+        ans = s + s
+        return goal in ans
+        
