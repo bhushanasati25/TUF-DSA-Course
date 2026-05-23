08f96d964e9e9a5f97344a128e68e971f36b8764
Bhushan Asati <bhushanasati25@gmail.com>
2026-05-23 12:50:19 -0400

    Code Updated

---
+++ Daily Problem/Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        cnt = 0
+
+        for i in range(1, n):
+            if nums[i - 1] > nums[i]:
+                cnt += 1
+        
+        if nums[-1] > nums[0]:
+            cnt += 1
+
+        return cnt <= 1
+
