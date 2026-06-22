55d9b6edc1c667020821eefec59dd52bab4032a0
Bhushan Asati <bhushanasati25@gmail.com>
2026-06-22 04:12:39 -0400

    Code Updated

---
+++ Daily Problem/Maximum Number of Balloons.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        count = Counter(text)
+        
+        return min(
+            count['b'], 
+            count['a'], 
+            count['l'] // 2, 
+            count['o'] // 2, 
+            count['n']
+        )
